26f696c7721065f0a406dcb5948669797e801f49
eliasg52 <eliasgarcia81@gmail.com>
2020-11-14 01:19:28 -0300

    funcionabilidades agregadas
    Cree las funciones para poder borrar las listas y ademas cree la funcion para poder agregar tareas

---
+++ ToDo-App/js/app.js
@@ -0,0 +1,58 @@
+const $listaTareas = document.querySelector('#tasks-list');
+const $Input = document.querySelector('#input-task');
+const $iconoAgregarTarea = document.querySelector('#icon-add-task');
+const $iconoBorrarLista = document.querySelector('#delete-list');
+
+$iconoAgregarTarea.onclick = function () {
+  const valorInput = $Input.value;
+  if (valorInput === '') {
+    advertencia(valorInput);
+  } else {
+    insertarElementos(valorInput);
+    $Input.value = '';
+  }
+};
+
+$iconoBorrarLista.onclick = function () {
+  const $tareas = document.querySelectorAll('.task-list');
+  borrarListaCompleta($tareas);
+};
+
+$Input.addEventListener('keypress', function (e) {
+  const valorInput = $Input.value;
+  if (e.key === 'Enter') {
+    insertarElementos(valorInput);
+    e.currentTarget.value = '';
+  } else {
+    return '';
+  }
+  e.preventDefault();
+});
+
+function insertarElementos(valorInput) {
+  $listaTareas.insertAdjacentHTML(
+    'beforeend',
+    `<div id="task" class="task-list">
+        <li class="ml-3">${valorInput}</li>
+            <div>
+                <i class="fas fa-check mr-1"></i>
+                <i class="fas fa-times mr-1"></i>
+            </div>
+    </div>`
+  );
+}
+
+function borrarListaCompleta(tareas) {
+  for (let i = 0; i < tareas.length; i++) {
+    $listaTareas.removeChild(tareas[i]);
+  }
+}
+
+function borrarInput(valorInput) {
+  valorInput.value = '';
+}
+
+function advertencia(valorInput) {
+  console.log(valorInput);
+  return;
+}
